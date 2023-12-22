4f90e2fa20d870f5c899da0366ef9544100f6fed
zorojuro <smasayedalbanna75@gmail.com>
2023-12-23 01:29:11 +0600

    button cursor

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Resume = (props: Props) => {
   return (
-    <div className="w-fit text-sm border py-5 px-10 rounded-full border-gray-600 hover:bg-[#2d2f3b] text-gray-500 hover:text-gray-200 flex gap-3 items-center justify-center shadow-lg hover:shadow-2xl">
+    <div className="w-fit text-sm border py-5 px-10 rounded-full border-gray-600 hover:bg-[#2d2f3b] text-gray-500 hover:text-gray-200 flex gap-3 items-center justify-center shadow-lg hover:shadow-2xl cursor-pointer">
         Get Resume
         <FaDownload />
       </div>
